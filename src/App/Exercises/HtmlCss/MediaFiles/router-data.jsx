dee60f4e67cfa40599a17f05d7649aaf8eeb2803
Marianna Palka <marianna.palka02@gmail.com>
2023-07-23 22:21:04 +0200

    Dodano sekcję o plikach mulitmedialnych.

---
+++ src/App/Exercises/HtmlCss/MediaFiles/router-data.jsx
@@ -0,0 +1,10 @@
+import { MediaFiles } from './index';
+
+export const MediaMetaData = {
+  path: 'media',
+  date: '15 lipca 2023',
+  linkLabel: 'Pliki multimedialne',
+  blockNo: 1,
+  element: <MediaFiles />,
+  tags: ['html', 'media', 'audio', 'video', 'iframe'],
+};
